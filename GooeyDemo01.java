da4543e19d8396449f22eb2e39d1fddebbfc0525
tofr <mykolyk@stuycs.org>
2014-01-07 17:13:40 -0500

    update gooeydemo01

--- GooeyDemo01.java
+++ GooeyDemo01.java
@@ -1,5 +1,5 @@
 /* ===========================================
-   Demo class for graphical user interface (GUI) basics in the .
+   Demo class for graphical user interface (GUI) basics using Swing.
    ===========================================*/
 
 import javax.swing.*;
@@ -8,12 +8,15 @@ public class GooeyDemo01 extends JFrame {
 
     public GooeyDemo01() {
 
-	//usage: Uncomment 1 line at a time, compile, run.
-	//Observe. 
-	//Modify inputs, recompile, run.
-	//Observe.
-	//Q: What does each statement do? What do the inputs do?
-	//        setTitle( "GUI Demo 01" );
+	/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	  usage: Uncomment 1 line at a time, compile, run.
+	  Observe, make notes.
+	  Modify inputs, recompile, run.
+	  Observe, make notes.
+	  Q: What does each statement do? What do the inputs do?
+	  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+	//setTitle( "GUI Demo 01" );
         //setSize( 800, 200 );
         //setLocation( 100, 100 );
         //setDefaultCloseOperation( EXIT_ON_CLOSE );
