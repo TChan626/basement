d101a21169c7b1a5e93926b67218098dbae467de
tofr <mykolyk@stuycs.org>
2014-01-07 17:07:36 -0500

    add GooeyDemo01.java

---
+++ GooeyDemo01.java
@@ -0,0 +1,29 @@
+/* ===========================================
+   Demo class for graphical user interface (GUI) basics in the .
+   ===========================================*/
+
+import javax.swing.*;
+
+public class GooeyDemo01 extends JFrame {
+
+    public GooeyDemo01() {
+
+	//usage: Uncomment 1 line at a time, compile, run.
+	//Observe. 
+	//Modify inputs, recompile, run.
+	//Observe.
+	//Q: What does each statement do? What do the inputs do?
+	//        setTitle( "GUI Demo 01" );
+        //setSize( 800, 200 );
+        //setLocation( 100, 100 );
+        //setDefaultCloseOperation( EXIT_ON_CLOSE );
+    }
+
+
+    public static void main( String[] args ) {
+
+        GooeyDemo01 g = new GooeyDemo01();
+	//g.setVisible( true );
+    }
+
+}
