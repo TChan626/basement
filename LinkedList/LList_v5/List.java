0bd5fc0e6006055c860a9cb902de1826f5240ce7
tofr <mykolyk@stuycs.org>
2014-04-01 12:57:19 -0400

    add linked list version 5

---
+++ LinkedList/LList_v5/List.java
@@ -0,0 +1,33 @@
+/*****************************************************
+ * interface List
+ * Specifies actions a List must be able to perform.
+ * New in Version 5: Explicit iteration
+ *****************************************************/
+
+import java.util.Iterator;
+
+public interface List<T>{ 
+
+    //add element T to end of list
+    //always return true
+    public boolean add( T x ); 
+
+    //insert element T at index i
+    public void add( int i, T newVal );
+
+    //remove element at index i
+    public T remove( int i );
+
+    //return element at index i
+    public T get( int i ); 
+
+    //overwrite element at index i, return old element at index i
+    public T set( int i, T x ); 
+
+    //return number of meaningful elements in list
+    public int size(); 
+
+    //return an Iterator over the elements in list
+    Iterator<T> iterator();
+
+}//end interface List
